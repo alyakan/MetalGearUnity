ce1fb2f6a0b94e060b5ab7ee5a875d5f43891c8f
kareemtarek4 <kareem.tarek04@gmail.com>
2016-12-20 08:19:15 +0200

    multiple enemies

--- Assets/Scripts/KeyPickup.cs
+++ Assets/Scripts/KeyPickup.cs
@@ -5,9 +5,14 @@ public class KeyPickup : MonoBehaviour {
 	private GameObject player;
 	private PlayerInventory playerInventory;
 
+	private GameObject door;
+	private Light doorSpotLight;
+
 	// Use this for initialization
 	void Awake () {
 		player = GameObject.FindGameObjectWithTag(Tags.player);
+		door =  GameObject.FindGameObjectWithTag(Tags.door);
+		doorSpotLight =  GameObject.FindGameObjectWithTag(Tags.doorSpotLight).GetComponent<Light>();
 		playerInventory = player.GetComponent<PlayerInventory> ();
 
 	
@@ -18,7 +23,11 @@ public class KeyPickup : MonoBehaviour {
 		if (other.gameObject == player) {
 			print ("######### keyPickedUp #########");
 			playerInventory.hasKey = true;
+			// pick up key
 			Destroy (gameObject);
+			// open door
+			doorSpotLight.color = Color.green;
+			Destroy (door);
 			
 		}
 	
--- Assets/Scripts/Unapplied/Tags.cs
+++ Assets/Scripts/Unapplied/Tags.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Tags : MonoBehaviour {
 
 	public const string player = "Player";
+	public const string door = "Door";
+	public const string doorSpotLight = "Door Light";
 	public const string enemy = "Enemy";
 	public const string gameController = "GameController";
 	public const string fader = "Fader";
